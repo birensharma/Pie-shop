faaf26bc49a010cc4075bc4d4e7b7953f8b20f47
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2019-05-01 23:40:23 +0530

    some files

---
+++ gallery.php
@@ -0,0 +1,147 @@
+<!Doctype html>
+
+<html lang="en">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<meta name="description" content="" />
+
+<meta name="keywords" content="" />
+
+<head>
+	
+<title>MFORS</title>
+
+<link rel="stylesheet" href="css/main.css" />
+
+<link rel="stylesheet" href="css/lightbox.min.css" />
+
+<script src="js/jquery.min.js" ></script>
+
+<script src="js/myscript.js"></script>
+	
+</head>
+
+<body>
+	
+<?php require "includes/header.php"; ?>
+
+<div class="parallax" onclick="remove_class()">
+	
+	<div class="parallax_head">
+		
+		<h2>Our</h2>
+		<h3>Gallery</h3>
+		
+	</div>
+	
+</div>
+
+<div class="content" onclick="remove_class()">
+	
+	<div class="inner_content on_parallax">
+		
+		<h2><span class="fresh">Food Varieties</span></h2>
+		
+		<div class="parallax_content">
+			
+			<div class="multicol">
+				
+				<div class="image_display">
+				
+					<a href="image/dish_3.jpg" data-lightbox="image-1"><img src="image/dish_3.jpg" alt="image/dish_3.jpg" width="100%" /></a>
+					
+				</div>
+				
+				<div class="image_display">
+					
+					<a href="image/dish.jpg" data-lightbox="image-2"><img src="image/dish.jpg" alt="image/dish.jpg" width="100%" /></a>
+					
+				</div>
+				
+				<div class="image_display">
+					
+					<a href="image/dish_4.jpg" data-lightbox="image-3"><img src="image/dish_4.jpg" alt="image/dish_4.jpg" width="100%" />
+					
+				</div>
+				
+				<div class="image_display">
+					
+					<a href="image/dish_2.jpg" data-lightbox="image-4"><img src="image/dish_2.jpg" alt="image/dish_2.jpg" width="100%" /></a>
+					
+				</div>
+				
+				<div class="image_display">
+					
+					<a href="image/dish_5.jpg" data-lightbox="image-5"><img src="image/dish_5.jpg" alt="image/dish_5.jpg" width="100%" /></a>
+					
+				</div>
+                    
+                <div class="image_display">
+					
+					<a href="image/dish_6.jpg" data-lightbox="image-6"><img src="image/dish_6.jpg" alt="image/dish_6.jpg" width="100%" /></a>
+					
+				</div>
+                    
+                <div class="image_display">
+					
+					<a href="image/dish_7.jpg" data-lightbox="image-7"><img src="image/dish_7.jpg" alt="image/dish_7.jpg" width="100%" /></a>
+					
+				</div>
+				
+			</div>
+			
+			<p class="clear"></p>
+			
+		</div>
+		
+	</div>
+	
+</div>
+
+<div class="on_footer_parallax">
+			<div class="content_footer" onclick="remove_class()">
+
+				<div class="inner_content">
+
+					<div class="contact">
+
+						<div class="left">
+
+							<h3>LOCATION</h3>
+							<p>Niit University</p>
+							<p>NH 8, Delhi- Jaipur Highway, District Alwar, Neemrana, Rajasthan 301705</p>
+
+						</div>
+
+						<div class="left">
+
+							<h3>CONTACT</h3>
+							<p>9928115635</p>
+							<p>local.pieshop@yahoo.com</p>
+
+						</div>
+					</div>
+
+				</div>
+
+				<div class="footer_copyright">
+					<div>
+						<p>&copy; <?php echo strftime("%Y", time()); ?> <span>MyRestaurant</span>. All Rights Reserved</p>
+					</div>
+					<div class="icon_holder">
+						<a href="#"><img src="image/icons/Facebook.png" alt="image/icons/Facebook.png" /></a>
+						<a href="#"><img src="image/icons/Google+.png" alt="image/icons/Google+.png" /></a>
+						<a href="#"><img src="image/icons/Twitter.png" alt="image/icons/Twitter.png" /></a>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+
+</body>
+
+</html>
+
+<script src="js/lightbox.min.js" ></script>
